65f8aafaec124878006cfe2bb3972b3ebc43a863
ptakondrej <florixakcz@seznam.cz>
2026-07-01 14:53:55 +0200

    chore: add schema barrel exports

---
+++ src/database/schema/index.ts
@@ -0,0 +1,14 @@
+export { characters } from './characters.schema';
+export { elements } from './elements.schema';
+export { locations } from './locations.schema';
+export { realms } from './realms.schema';
+export { seasons } from './seasons.schema';
+export { weapons } from './weapons.schema';
+export {
+  charactersToElements,
+  charactersToSeasons,
+  charactersToWeapons,
+  locationsToSeasons,
+  weaponsToSeasons,
+} from './junctions.schema';
+export { users } from './users.schema';
