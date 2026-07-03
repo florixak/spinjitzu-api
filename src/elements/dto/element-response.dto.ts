b5f315b7d15487c358b969dff5ec1b9ec44eca7b
ptakondrej <florixakcz@seznam.cz>
2026-07-03 18:23:44 +0200

    feat: add masters type to element detail dto

--- src/elements/dto/element-response.dto.ts
+++ src/elements/dto/element-response.dto.ts
@@ -1,12 +1,24 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { CharacterListItemDto } from 'src/characters/dto/character-response.dto';
 
 export class ElementListItemDto {
   @ApiProperty() id: number;
   @ApiProperty() name: string;
-  @ApiProperty() description: string;
 }
 
 export class ElementDetailDto extends ElementListItemDto {
   @ApiProperty() createdAt: Date;
   @ApiProperty() updatedAt: Date;
+  @ApiPropertyOptional({
+    type: String,
+    nullable: true,
+    example: 'The elemental power of fire...',
+  })
+  description: string | null;
+  @ApiProperty({
+    type: [CharacterListItemDto],
+    description:
+      'List of element masters that have this element or had it in the past',
+  })
+  masters: CharacterListItemDto[];
 }
