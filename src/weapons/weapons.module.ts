41b63f9b45aaa4421f621c8394e9cf1b71790b59
ptakondrej <florixakcz@seznam.cz>
2026-07-04 11:12:52 +0200

    feat: import Database and Auth module

--- src/weapons/weapons.module.ts
+++ src/weapons/weapons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeaponsService } from './weapons.service';
 import { WeaponsController } from './weapons.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule, AuthModule],
   controllers: [WeaponsController],
   providers: [WeaponsService],
 })
