c733016de491011f7279cb1c5db0205c4abd1e97
ptakondrej <florixakcz@seznam.cz>
2026-07-04 07:25:47 +0200

    feat: import Database and Auth module

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { LocationsService } from './locations.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { LocationsController } from './locations.controller';
+import { LocationsService } from './locations.service';
 
 @Module({
+  imports: [DatabaseModule, AuthModule],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
