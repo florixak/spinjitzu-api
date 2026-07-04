3a1cfc2d46b7e1dfa1b8c226a5fd3787bce2f7ef
ptakondrej <florixakcz@seznam.cz>
2026-07-04 19:12:15 +0200

    feat: add throttle for health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { HealthCheck } from '@nestjs/terminus';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 import { HealthService } from './health.service';
 
 @ApiTags('health')
@@ -9,7 +10,10 @@ export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Check the health of the database' })
   @HealthCheck()
+  @SkipThrottle()
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   check() {
     return this.healthService.checkDatabase();
   }
